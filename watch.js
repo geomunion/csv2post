9665af0389c1a0bd86ee0a5eb6a2cedf564e5fe6
Tobias Wawryniuk <tobias@emerald.network.lan>
2017-03-09 19:49:29 +0100

    add watcher script

---
+++ watch.js
@@ -0,0 +1,43 @@
+
+'use strict';
+
+const sane = require('sane');
+
+const Reader = require('./lib/reader.js');
+
+var config = require('./lib/config.js')('config.ini').config;
+
+
+// console.log(config);
+
+var reader = new Reader(config);
+
+var invoke = function(filepath) {
+    if (reader.isRunning == false ) {
+	reader.load(filepath);
+    } else {
+	console.log('already running');
+    }
+}
+
+//var watcher = sane('path/to/dir', {glob: ['**/*.js', '**/*.css']});
+var watcher = sane(config.importDir, {glob: config.watcher.file});
+
+watcher.on('ready', function () { console.log('ready') });
+
+watcher.on('change', function (filepath, root, stat) {
+    console.log('file changed', root + '/' + filepath);
+    invoke( root + '/' + filepath );
+});
+
+watcher.on('add', function (filepath, root, stat) {
+    console.log('file added', filepath);
+    invoke( root + '/' + filepath );
+});
+
+watcher.on('delete', function (filepath, root) {
+    console.log('file deleted', filepath);
+});
+
+// close
+// watcher.close();
